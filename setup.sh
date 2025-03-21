b3bf590a39d109ada590dbb26cd8d7d4aaff734e
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-03-21 22:08:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -197,7 +197,7 @@ cd ..
 # Reload gnome shell
 echo ">> Reloading gnome shell..."
 if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
-    killall -HUP gnome-shell &> /dev/null
+    dbus-launch killall -HUP gnome-shell &> /dev/null
 fi
 
 # Enable extensions
@@ -219,7 +219,7 @@ if [ "$INSTALL_SEARCH_LIGHT" == "true" ]; then
 fi
 
 if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
-    killall -HUP gnome-shell &> /dev/null
+    dbus-launch killall -HUP gnome-shell &> /dev/null
 fi
 ########################################### EXTENSIONS ###########################################
 
